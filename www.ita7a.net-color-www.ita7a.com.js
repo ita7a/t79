5e39ffa8e2da55401b2585c0f58ef6eabc27599e
ita7a <57022144+ita7a@users.noreply.github.com>
2019-11-17 03:12:00 +0300

    Create www.ita7a.net-color-www.ita7a.com.js

---
+++ www.ita7a.net-color-www.ita7a.com.js
@@ -0,0 +1,72 @@
+.nav-tabsinsertBefore<img src="" class="fr" style="width:100%;margin-right: 2px;">
+<a href="" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini fa fa-star-half-o btn btn-info"></a>
+
+<a href="" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini fa fa-bell btn btn-danger"></a> 
+
+<a href="" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini fa fa-heartbeat btn btn-info"></a> 
+
+ <form name="jump">
+<div class="MyColors">
+					<map id="ColorsMap" name="ColorsMap">
+					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
+					<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2">
+					<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3">
+					<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4">
+					<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
+					<area alt="" coords="122, 0, 147, 25" href="javascript:chooseStyle('Co6',60)" shape="rect" title="6">
+					<area alt="" coords="92, 0, 117, 25" href="javascript:chooseStyle('Co7',60)" shape="rect" title="7">
+					</map>
+			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>
+</form>
+
+<div style="border-radius: 2px 9px 9px 9px;width:100%;margin:2px;border: 2px solid #8F0000;" id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">  </div><div class="collapse" id="collapseExample"><center><div class="well">
+<b>
+<font color="#78939C">   </font> </BR>
+</BR> Deem , </BR>
+</BR>   </BR>
+<font color="#BF1D6E"></font></BR>
+
+length.pmsgcdiv#d2appendTo<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:#F7E7E7;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url;" class="fl fitimg hand u-pic ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"> .. ' ..    </span>
+</div>
+<br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg break fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg break fl">      <h1 style="display: inline-block;color: red;">topic</h1>           </div>
+</div>
+</div></div>
+
+logclickdiv#tlogins button.btn.btn-primaryhttps://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.jsgetScriptwidth100%cssfladdClasshrefremoveAttra[href="https://jawalhost.com/"]body<link rel="stylesheet" type="text/css" href="">
+
+<link href="https://rawcdn.githack.com/waleed-qaid/des/c4032f6c7d1463651276473d50f3de30ff84d176/www.1des.net-color1-www.skr0.com.css" rel="stylesheet" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color2-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color3-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color4-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color5-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/des/c272a7d15ea3a4f1a2375250a30bdca80fae8970/www.1des.net-color6-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co6" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color7-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co7" type="text/css" />
+<style>
+
+.MyColors {
+    position: absolute;
+    transform:matrix(1, 0, 0, 1, 79, 5);
+	margin-top: 2px;
+    width: 300px;
+    height: 40px;
+    float: left;
+    background: transparent url(https://skr0.com/sico/z1dplbn6u910.png) no-repeat scroll right top;
+    text-align: left;
+}
+
+a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh {
+    display: none
+}
+
+</style>
+#d2<center><div><div width="99.5%" style="background-image:url(https://skr0.com/sico/z1dorkl9fe10.jpg);color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ABABAB;border-top: 2px solid #ABABAB;padding-bottom: 2px!important;" ><font style="background-color: #CCC0D1;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >     </font></div></div><center>a[href="https://skr0.com/sico/z1dorklsaa10.jpg"]span.fl.fa.fa-send<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="assl.css">
+</style>.loginstatinsertAfter<a href=' ' target='_blank' class='fr btn btn-danger fa fa-star'>...... .......</a>span.s1.fa.fa-user.label.badgex.label-as-badge.label-success<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;color:#FFF7AB;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">      
+</marquee></font></b></td>
